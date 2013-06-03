8b67cc297274dad7259400a4d4938079e8e5c3e5
Louis Northmore <louis.northmore@gmail.com>
2013-06-03 11:58:08 +0100

    Update

--- paypal-button-store.php
+++ paypal-button-store.php
@@ -10,6 +10,7 @@ Author URI: http://interactivedimension.com
 
 function spbs_core() {
 
+
 //products post type
     $labels = array(
         'name' => 'Products',
@@ -39,10 +40,73 @@ function spbs_core() {
         'has_archive' => true,
         'hierarchical' => false,
         'menu_position' => null,
-        'supports' => array( 'title', 'editor', 'author', 'thumbnail' )
+        'supports' => array( 'title', 'editor', 'thumbnail' )
     );
 
     register_post_type( 'spbs-product', $args );
 
+
+    add_action( 'save_post', 'spbs_save_postdata' );
+
 }
 add_action('init', 'spbs_core');
+
+function spbs_product_meta() {
+//product meta
+    $screens = array( 'spbs-product' );
+    foreach ($screens as $screen) {
+        add_meta_box(
+            'myplugin_sectionid',
+            __( 'Product Information', 'myplugin_textdomain' ),
+            'spbs_product_meta_box',
+            $screen
+        );
+    }
+
+
+}
+add_action('add_meta_boxes', 'spbs_product_meta');
+
+function spbs_product_meta_box() {
+
+    // Use nonce for verification
+    wp_nonce_field( plugin_basename( __FILE__ ), 'myplugin_noncename' );
+
+    // The actual fields for data entry
+    // Use get_post_meta to retrieve an existing value from the database and use the value for the form
+    $value = get_post_meta( $post->ID, '_my_meta_value_key', true );
+    echo '<label for="myplugin_new_field">';
+    _e("Description for this field", 'myplugin_textdomain' );
+    echo '</label> ';
+    echo '<input type="text" id="myplugin_new_field" name="myplugin_new_field" value="'.esc_attr($value).'" size="25" />';
+}
+
+/* When the post is saved, saves our custom data */
+function spbs_save_postdata( $post_id ) {
+
+    // First we need to check if the current user is authorised to do this action.
+    if ( 'page' == $_POST['post_type'] ) {
+        if ( ! current_user_can( 'edit_page', $post_id ) )
+            return;
+    } else {
+        if ( ! current_user_can( 'edit_post', $post_id ) )
+            return;
+    }
+
+    // Secondly we need to check if the user intended to change this value.
+    if ( ! isset( $_POST['myplugin_noncename'] ) || ! wp_verify_nonce( $_POST['myplugin_noncename'], plugin_basename( __FILE__ ) ) )
+        return;
+
+    // Thirdly we can save the value to the database
+
+    //if saving in a custom table, get post_ID
+    $post_ID = $_POST['post_ID'];
+    //sanitize user input
+    $mydata = sanitize_text_field( $_POST['myplugin_new_field'] );
+
+    // Do something with $mydata
+    // either using
+    add_post_meta($post_ID, '_my_meta_value_key', $mydata, true) or
+    update_post_meta($post_ID, '_my_meta_value_key', $mydata);
+    // or a custom table (see Further Reading section below)
+}
