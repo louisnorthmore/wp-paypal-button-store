d80b3b9a0f8f31d933040126426dc1a5d50d5611
Louis Northmore <louis.northmore@gmail.com>
2013-06-03 12:43:43 +0100

    Update

--- paypal-button-store.php
+++ paypal-button-store.php
@@ -74,7 +74,7 @@ function spbs_product_meta_box($post) {
     $value = get_post_meta( $post->ID, 'spbs_paypal_button_code', true );
 
 
-    echo 'PayPal Button Code:<br><textarea rows="8" cols="140" id="spbs_paypal_button_code" name="spbs_paypal_button_code">'.$value.'</textarea>';
+    echo '<p><h3>PayPal Button Code:<h3><textarea rows="8" cols="140" id="spbs_paypal_button_code" name="spbs_paypal_button_code">'.$value.'</textarea></p>';
 }
 
 /* When the post is saved, saves our custom data */
