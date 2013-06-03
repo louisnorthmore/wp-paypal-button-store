d1c525e5a5f9c4fa02ff794046301f77c06681ef
Louis Northmore <louis.northmore@gmail.com>
2013-06-03 10:56:41 +0100

    Initial Plugin File

---
+++ paypal-button-store.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Simple PayPal Button Store
+Plugin URI: http://interactivedimension.com
+Description: WordPress simple store for PayPal Button items
+Version: 1.0
+Author: @weareid, @louisnorthmore
+Author URI: http://interactivedimension.com
+*/
