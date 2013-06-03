4083d72913221b5832eaeecab35068148c1073b0
Louis Northmore <louis.northmore@gmail.com>
2013-06-03 11:47:49 +0100

    Added Products post type

--- paypal-button-store.php
+++ paypal-button-store.php
@@ -7,3 +7,42 @@ Version: 1.0
 Author: @weareid, @louisnorthmore
 Author URI: http://interactivedimension.com
 */
+
+function spbs_core() {
+
+//products post type
+    $labels = array(
+        'name' => 'Products',
+        'singular_name' => 'Product',
+        'add_new' => 'Add New',
+        'add_new_item' => 'Add New Product',
+        'edit_item' => 'Edit Product',
+        'new_item' => 'New Product',
+        'all_items' => 'All Products',
+        'view_item' => 'View Product',
+        'search_items' => 'Search Products',
+        'not_found' =>  'No products found',
+        'not_found_in_trash' => 'No products found in Trash',
+        'parent_item_colon' => '',
+        'menu_name' => 'Products'
+    );
+
+    $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'publicly_queryable' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'query_var' => true,
+        'rewrite' => array( 'slug' => 'product' ),
+        'capability_type' => 'post',
+        'has_archive' => true,
+        'hierarchical' => false,
+        'menu_position' => null,
+        'supports' => array( 'title', 'editor', 'author', 'thumbnail' )
+    );
+
+    register_post_type( 'spbs-product', $args );
+
+}
+add_action('init', 'spbs_core');
